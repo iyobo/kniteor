77d256d0dcdd715379b1f81f55ff5254251d346b
iyobo Eki <phil.eki@gmail.com>
2016-06-18 16:07:26 -0700

    Documenting

--- imports/bridge/link.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by iyobo on 2016-06-18.
- */
--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
 npm install -g meteor
-meteor add cfs:standard-packages
-meteor add cfs:gridfs
-meteor add reactive-dict
-meteor add accounts-ui accounts-password
